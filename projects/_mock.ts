2013328b832ab2974dc5858787fe1a19831b19de
Omochice <mochice.mls.ntl@gmail.com>
2025-06-01 03:27:20 +0900

    test: refactor tests for projects

---
+++ projects/_mock.ts
@@ -0,0 +1,185 @@
+import { http, HttpResponse } from "npm:msw@2.8.7";
+import { STATUS_CODE } from "jsr:@std/http@1.0.17/status";
+
+export const context = {
+  apiKey: "sample",
+  endpoint: "http://redmine.example.com",
+};
+
+export const validHandlers = [
+  http.put(`${context.endpoint}/projects/:id/archive.json`, () => {
+    return HttpResponse.json({});
+  }),
+  http.put(`${context.endpoint}/projects/:id/unarchive.json`, () => {
+    return HttpResponse.json({});
+  }),
+  http.post(`${context.endpoint}/projects.json`, () => {
+    return HttpResponse.json({});
+  }),
+  http.delete(`${context.endpoint}/projects/:id.json`, () => {
+    return HttpResponse.json({});
+  }),
+  http.get(`${context.endpoint}/projects.json`, () => {
+    const projects = [
+      {
+        id: 3,
+        name: "sample3",
+        identifier: "sample3",
+        description: "sample project 3",
+        homepage: undefined,
+        status: 1,
+        is_public: true,
+        inherit_members: false,
+        enable_new_ticket_message: undefined,
+        new_ticket_message: undefined,
+        default_version: undefined,
+        created_on: "1970-03-01t00:00:00.000z",
+        updated_on: "1971-03-01t00:00:00.000z",
+        parent: undefined,
+      },
+      {
+        id: 2,
+        name: "sample2",
+        identifier: "sample2",
+        description: null,
+        homepage: undefined,
+        status: 1,
+        is_public: true,
+        inherit_members: false,
+        enable_new_ticket_message: undefined,
+        new_ticket_message: undefined,
+        default_version: undefined,
+        created_on: "1970-02-01t00:00:00.000z",
+        updated_on: "1971-02-01t00:00:00.000z",
+        parent: undefined,
+      },
+      {
+        id: 1,
+        name: "sample project name",
+        identifier: "sample",
+        description: "sample project",
+        homepage: undefined,
+        status: 1,
+        is_public: true,
+        inherit_members: false,
+        enable_new_ticket_message: undefined,
+        new_ticket_message: undefined,
+        default_version: undefined,
+        created_on: "1970-01-01t00:00:00.000z",
+        updated_on: "1971-01-01t00:00:00.000z",
+        parent: undefined,
+      },
+    ] as const;
+    return HttpResponse.json({
+      projects,
+      total_count: projects.length,
+      offset: 0,
+      limit: 25,
+    }, {
+      status: 200,
+    });
+  }),
+  http.get(`${context.endpoint}/projects/:id.json`, () => {
+    const project = {
+      id: 1,
+      name: "sample project name",
+      identifier: "sample",
+      description: "sample project",
+      homepage: "",
+      status: 1,
+      is_public: true,
+      inherit_members: false,
+      enable_new_ticket_message: undefined,
+      new_ticket_message: undefined,
+      default_version: undefined,
+      created_on: "1970-01-01T00:00:00.000Z",
+      updated_on: "1971-01-01T00:00:00.000Z",
+      parent: undefined,
+    } as const;
+    return HttpResponse.json({ project });
+  }),
+  http.put(`${context.endpoint}/projects/:id.json`, () => {
+    return HttpResponse.json({});
+  }),
+];
+
+export const invalidHandlers = [
+  http.put(`${context.endpoint}/projects/422/archive.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.put(`${context.endpoint}/projects/404/archive.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+  http.put(`${context.endpoint}/projects/422/unarchive.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.put(`${context.endpoint}/projects/404/unarchive.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+  http.post(`${context.endpoint}/422/projects.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.post(`${context.endpoint}/404/projects.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+  http.delete(`${context.endpoint}/projects/422.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.delete(`${context.endpoint}/projects/404.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+  http.get(`${context.endpoint}/422/projects.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.get(`${context.endpoint}/404/projects.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+  http.put(`${context.endpoint}/projects/422.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.get(`${context.endpoint}/projects/422.json`, () => {
+    return HttpResponse.json({
+      errors: ["sample error"],
+    }, {
+      status: STATUS_CODE.UnprocessableEntity,
+      statusText: "Unprocessable Entity",
+    });
+  }),
+  http.get(`${context.endpoint}/projects/404.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+
+  http.put(`${context.endpoint}/projects/404.json`, () => {
+    return new HttpResponse(null, { status: STATUS_CODE.NotFound });
+  }),
+];
--- projects/create.test.ts
+++ projects/create.test.ts
@@ -1,18 +1,19 @@
 import { create } from "./create.ts";
 import { assert } from "jsr:@std/assert@1.0.13";
-import { contexts, handler } from "./create.mock.ts";
+import { context, invalidHandlers, validHandlers } from "./_mock.ts";
 import { setupServer } from "npm:msw@2.8.7/node";
 
-const server = setupServer(...handler);
+const server = setupServer();
 server.listen();
 
-Deno.test("test for redmine project 'create' endpoint", async (t) => {
+Deno.test("POST /projects.json", async (t) => {
   await t.step(
     "if got 200, should be success",
     async () => {
+      server.use(...validHandlers);
       const e = await create(
         { name: "sample", identifier: "sample" },
-        contexts[0],
+        context,
       );
       assert(e.isOk());
     },
@@ -21,19 +22,17 @@ Deno.test("test for redmine project 'create' endpoint", async (t) => {
   await t.step(
     "if get invalid response with error object, should be err with error text",
     async () => {
-      const e = await create(
-        { name: "sample", identifier: "sample" },
-        contexts[1],
-      );
+      server.use(...invalidHandlers);
+      const c = { ...context, endpoint: `${context.endpoint}/422` };
+      const e = await create({ name: "sample", identifier: "sample" }, c);
       assert(e.isErr());
     },
   );
 
   await t.step("if get invalid response with unexpected format", async () => {
-    const e = await create(
-      { name: "sample", identifier: "sample" },
-      contexts[2],
-    );
+    server.use(...invalidHandlers);
+    const c = { ...context, endpoint: `${context.endpoint}/422` };
+    const e = await create({ name: "sample", identifier: "sample" }, c);
     assert(e.isErr());
   });
 });
