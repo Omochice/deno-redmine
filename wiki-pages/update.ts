b31d1867583ab1277245f819782d2d7620bda5a3
Omochice <mochice.mls.ntl@gmail.com>
2025-06-01 02:23:34 +0900

    fixup! feat: add update

--- wiki-pages/update.ts
+++ wiki-pages/update.ts
@@ -3,7 +3,7 @@ import { ResultAsync } from "npm:neverthrow@8.2.0";
 import { convertError } from "../error.ts";
 
 /**
- * Create a wiki page in the project
+ * Update a wiki page in the project
  *
  * @param context REST endpoint context
  * @param projectId Project identifier
