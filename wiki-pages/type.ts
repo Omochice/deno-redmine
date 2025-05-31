49643e784cf9b94bcc2adf5d498777ec254ec752
Omochice <mochice.mls.ntl@gmail.com>
2025-06-01 02:12:46 +0900

    feat: add type and validator

---
+++ wiki-pages/type.ts
@@ -0,0 +1,54 @@
+export type Wiki = {
+  title: string;
+  version: number;
+  createdOn: Date;
+  updatedOn: Date;
+  parent?: {
+    title: string;
+  };
+};
+
+export type WikiDetail = {
+  title: string;
+  version: number;
+  text: string;
+  author: Author;
+  comments: string;
+  createdOn: Date;
+  updatedOn: Date;
+  attachments?: Attachment[];
+};
+
+type Author = {
+  id: number;
+  name: string;
+};
+
+type Attachment = {
+  id: number;
+  filename: string;
+};
+
+export type WikiContent = {
+  title: string;
+  text: string;
+  comments?: string;
+  version?: number;
+  parentTitle?: string;
+};
+
+/** Characters that cannot be used in wiki title */
+const bannedCharacters = new Set([".", "/", "?", ";", ":", "|", " "]);
+
+/**
+ * Sanitize wiki title
+ *
+ * @param title sanitize target
+ * @returns Sanitized one
+ */
+export function sanitizeTitle(title: string): string {
+  return bannedCharacters.values().toArray().reduce(
+    (t, s) => t.replaceAll(s, "_"),
+    title,
+  );
+}
---
+++ wiki-pages/validator.ts
@@ -0,0 +1,106 @@
+import {
+  array,
+  custom,
+  number,
+  object,
+  optional,
+  pipe,
+  string,
+  transform,
+} from "jsr:@valibot/valibot@1.1.0";
+import type { SnakeCasedProperties } from "npm:type-fest@4.41.0";
+import {
+  sanitizeTitle,
+  type Wiki,
+  type WikiContent,
+  type WikiDetail,
+} from "./type.ts";
+// NOTE: replace valibot.toCamelCase when implments it
+import { objectToCamel } from "npm:ts-case-convert@2.1.0";
+
+const dateLikeString = pipe(
+  string(),
+  custom((input) => {
+    if (typeof input !== "string") {
+      return false;
+    }
+    return !isNaN(Date.parse(input));
+  }),
+  transform((input: string) => new Date(input)),
+);
+
+const inputWiki = object({
+  wiki_pages: array(object({
+    title: string(),
+    version: number(),
+    created_on: dateLikeString,
+    updated_on: dateLikeString,
+    parent: optional(
+      object({
+        title: string(),
+      }),
+    ),
+  })),
+});
+
+/**
+ * Validate that it is wiki object
+ */
+export const wikis = pipe(
+  inputWiki,
+  transform((input) => {
+    return input.wiki_pages
+      .map((w) => {
+        return objectToCamel(w) satisfies Wiki;
+      });
+  }),
+);
+
+const idName = object({
+  id: number(),
+  name: string(),
+});
+
+const inputWikiDetail = object({
+  wiki_page: object({
+    title: string(),
+    version: number(),
+    text: string(),
+    author: idName,
+    comments: string(),
+    created_on: dateLikeString,
+    updated_on: dateLikeString,
+    attachments: optional(
+      array(object({
+        id: number(),
+        filename: string(),
+      })),
+    ),
+  }),
+});
+
+/**
+ * Validate that it is detailed wiki object
+ */
+export const wikiDetail = pipe(
+  inputWikiDetail,
+  transform((input) => {
+    return objectToCamel(input.wiki_page) satisfies WikiDetail;
+  }),
+);
+
+/**
+ * Make wiki content to object for create/update
+ *
+ * @param wiki Wiki content
+ * @returns Converted one
+ */
+export function makeWikiPutRequest(wiki: WikiContent) {
+  const page = {
+    text: sanitizeTitle(wiki.text),
+    comments: wiki.comments,
+    version: wiki.version,
+    parent_title: wiki.parentTitle,
+  } satisfies Omit<SnakeCasedProperties<WikiContent>, "title">;
+  return { wiki_page: page };
+}
