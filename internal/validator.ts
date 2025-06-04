b02cfaee94627b04ae4afd199acb86a8a93a5ade
Omochice <mochice.mls.ntl@gmail.com>
2025-06-05 01:06:39 +0900

    refactor: move utility validator and type

---
+++ internal/validator.ts
@@ -0,0 +1,24 @@
+import {
+  custom,
+  number,
+  object,
+  pipe,
+  string,
+  transform,
+} from "jsr:@valibot/valibot@1.1.0";
+
+export const idName = object({
+  id: number(),
+  name: string(),
+});
+
+export const dateLikeString = pipe(
+  string(),
+  custom((input) => {
+    if (typeof input !== "string") {
+      return false;
+    }
+    return !isNaN(Date.parse(input));
+  }),
+  transform((input) => new Date(input)),
+);
