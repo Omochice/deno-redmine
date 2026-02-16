3fc7106328e38d515e52035f77d1d9b3ea09b6f2
Omochice <mochice.mls.ntl@gmail.com>
2026-02-17 00:49:06 +0900

    fix(versions): convert request body to snake_case in update
    Same issue as create: camelCase VersionQuery properties need to be
    converted to snake_case for the Redmine API to accept them.

--- throwable/versions/update.ts
+++ throwable/versions/update.ts
@@ -2,6 +2,7 @@ import { join } from "jsr:@std/path@1.1.4/posix/join";
 import type { Context } from "../../context.ts";
 import type { VersionQuery } from "./type.ts";
 import { assertResponse } from "../../error.ts";
+import { objectToSnake } from "npm:ts-case-convert@2.1.0";
 
 /**
  * Updates the version of given id.
@@ -22,7 +23,7 @@ export async function update(
       "Content-Type": "application/json",
       "X-Redmine-API-Key": context.apiKey,
     },
-    body: JSON.stringify({ version }),
+    body: JSON.stringify({ version: objectToSnake(version) }),
   });
   assertResponse(response);
 }
